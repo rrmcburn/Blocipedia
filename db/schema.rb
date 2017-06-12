57a8973f8e619c7bb0324ef1a88f233408ebe7aa
Ryan McBurney <rrmcburn@gmail.com>
2017-06-11 23:08:04 -0400

    quick fix with MyMailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612014258) do
+ActiveRecord::Schema.define(version: 20170612025324) do
 
   create_table "amounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "my_mailers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
